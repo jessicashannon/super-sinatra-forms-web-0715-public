45939106a18f692834ffce2ba7a10be311e5c68d
jessicashannon <jessdahling@gmail.com>
2015-08-26 16:02:54 -0400

    initial commit, tests passing

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :root, "#{settings.root}/."
+
+  get "/" do
+    erb :"form"
+  end
+  #
+  # get "/new" do
+  #   erb :"pirates/new"
+  # end
+  #
+  post "/teams" do
+    @team = Team.new(params)
+    erb :"team"
+  end
+
 
 end
---
+++ models/teams.rb
@@ -0,0 +1,6 @@
+class Team
+  attr_accessor :params
+  def initialize(params)
+    @params = params
+  end
+end
